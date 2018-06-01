dfdc788770223e9c9798748ff7c29f447dcd466c
Florin Ciurcanu <florin.ciurcanu@gmail.com>
2018-06-01 10:58:56 +0300

    added some fields

--- src/backapi/backpack/models.py
+++ src/backapi/backpack/models.py
@@ -6,9 +6,14 @@ import datetime
 class PrivatePerson(models.Model):
 	"""store sensitive information for a single person"""
 	full_name = models.CharField(max_length=254)
-	email = models.EmailField()
-	address = models.CharField(max_length=254)
-	phone = models.CharField(max_length=32)
+	phone = models.CharField(max_length=32, default='+40 ...')
+	email = models.EmailField(default='')
+	facebook_url = models.CharField(max_length=254, default='')
+	parent_phone = models.CharField(max_length=32, default='')
+	parent_email = models.EmailField(default='')
+	parent_facebook_url = models.CharField(max_length=254, default='')
+
+	address = models.CharField(max_length=254, default='')
 	joined = models.DateField(auto_now_add=True, auto_now=False)
 	last_modified = models.DateField(auto_now=True)
 
