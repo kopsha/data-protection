f251c2ad8406aee2db63437491795196fb961fda
Florin Ciurcanu <florin.ciurcanu@gmail.com>
2018-06-03 08:28:31 +0300

    reverted templates folder trick and preparing for deployment

--- src/backapi/home/urls.py
+++ src/backapi/home/urls.py
@@ -5,7 +5,7 @@ from django.views.generic.base import TemplateView
 import home.views
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login' ),
-    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='home/login.html'), name='login' ),
+    path('logout/', auth_views.LogoutView.as_view(template_name='home/logout.html'), name='logout'),
+    path('', TemplateView.as_view(template_name='home/home.html'), name='home'),
 ]
