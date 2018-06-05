8337b4855901af1d90631f6a548487b9c29fd673
Florin Ciurcanu <florin.ciurcanu@fortech.ro>
2018-06-05 16:21:38 +0300

    added static loader, template reorg completed

--- src/backapi/backpack/urls.py
+++ src/backapi/backpack/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
 	path('private_person/<int:id>/', backpack.views.private_details, name='view_person'),
 	path('private_person/<int:id>/update/', backpack.views.private_details, name='update_person'),
 	path('private_person/create/', backpack.views.private_details, name='create_person'),
+
 	path('user_home/', backpack.views.user_home, name='user_home'),
 
 	path('logout/', backpack.views.logout_view, name='logout'),
